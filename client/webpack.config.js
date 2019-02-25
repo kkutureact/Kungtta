4c16ba683a976f68e5fb6c7b53b5f60d6b88a31c
SkyLightQP <combbm@naver.com>
2019-02-25 13:54:13 +0900

    feat: apply React and Webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
+    },
+
+    devServer: {
+        inline: true,
+        port: 3000,
+        contentBase: __dirname + '/public/',
+        open: true
+    },
+
+    resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jp(e*)g)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8000,
+                    name: 'images/[hash]-[name].[ext]'
+                }
+            },
+            {
+                test: /\.svg$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            inject: true,
+            filename: './index.html'
+        })
+    ]
+};
