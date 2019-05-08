4f533c6fa76b861854b1cc26083bb122e3989f8e
SkyLightQP <combbm@naver.com>
2019-05-08 21:27:56 +0900

    fix: !==

--- server/src/game/commands/commandHandler.ts
+++ server/src/game/commands/commandHandler.ts
@@ -5,6 +5,6 @@ const commandList: { [k: string]: Command } = {};
 commandList.uuid = new CheckUuidCommand();
 
 export const CommandHandler = (command: string, args: string[], myself: string) => {
-    if(commandList[command] != undefined)
+    if(commandList[command] !== undefined)
         commandList[command].run(command, args, myself);
 };
